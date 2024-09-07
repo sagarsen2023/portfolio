cb1cb967d99fe5c59daa80fb7bf622c883aec17b
sagar Sen <sagarsen.dev@gmail.com>
2024-09-07 21:30:27 +0530

    Fixed navbar from absolute to sticky

--- src/components/Services-Section/ServicesSection.tsx
+++ src/components/Services-Section/ServicesSection.tsx
@@ -6,7 +6,9 @@ function ServicesSection({ services }: { services: ServicesSectionInterface }) {
   return (
     <div className="py-10">
       <h1 className="text-4xl text-center font-bold mt-10">{services.title}</h1>
-      <p className="text-center mt-4 mb-7 text-lg md:text-xl">{services.description}</p>
+      <p className="text-center mt-4 mb-7 text-lg md:text-xl">
+        {services.description}
+      </p>
       <div className="pt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 select-none">
         {services.services.map((service) => (
           <ServiceCard key={service.id} service={service} />
